756cb4f7db2f9ed1fdabc80da55af3a52f1bd699
Giordano Dìaz <diazgiordano@gmail.com>
2020-12-09 19:35:03 -0500

    Add seeds to the project

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |_i|
+  User.create!(name: Faker::Name.name,
+               username: Faker::Name.unique.first_name,
+               email: Faker::Internet.unique.email,
+               password: 'password',
+               password_confirmation: 'password')
+end
+
+Following.create(follower_id: 1, followed_id:10)
+Following.create(follower_id: 4, followed_id:3)
+Following.create(follower_id: 2, followed_id:7)
+Following.create(follower_id: 6, followed_id:4)
+
+Category.create(category: 'Ruby on Rails')
