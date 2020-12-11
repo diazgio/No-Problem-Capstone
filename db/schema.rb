410a8f9926ebe69c81c306cdeea6f10537aa5094
Giordano Dìaz <diazgiordano@gmail.com>
2020-12-11 14:38:33 -0500

    Fix one issue with schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
-# rubocop: disable Metrics/BlockLength, Layout/LineLength
+# rubocop: disable Metrics/BlockLength
 ActiveRecord::Schema.define(version: 202_012_071_516_17) do
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 202_012_071_516_17) do
     t.bigint 'record_id', null: false
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
-    t.index %W[record_type record_id name], name: 'index_action_text_rich_texts_uniqueness', unique: true
+    t.index %w[record_type record_id name], name: 'index_action_text_rich_texts_uniqueness', unique: true
   end
 
   create_table 'active_storage_attachments', force: :cascade do |t|
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 202_012_071_516_17) do
     t.bigint 'blob_id', null: false
     t.datetime 'created_at', null: false
     t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %W[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
+    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness', unique: true
   end
 
   create_table 'active_storage_blobs', force: :cascade do |t|
@@ -108,4 +108,4 @@ ActiveRecord::Schema.define(version: 202_012_071_516_17) do
   add_foreign_key 'followings', 'users', column: 'followed_id'
   add_foreign_key 'followings', 'users', column: 'follower_id'
 end
-# rubocop: enable Metrics/BlockLength, Layout/LineLength
+# rubocop: enable Metrics/BlockLength
